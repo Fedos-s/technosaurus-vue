41cdeeb08de40db5e9393b59d8c2ab0335b21116
Fedos-s <fedoseev-serge@yandex.ru>
2022-06-17 13:03:48 +0300

    second commit

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,49 @@
+<!-- eslint-disable linebreak-style -->
+<template>
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<!-- eslint-disable max-len -->
+<!-- eslint-disable -->
+  <li class="catalog__item">
+    <a class="catalog__pic" href="#">
+      <img :src="product.image" :alt="product.title">
+    </a>
+
+    <h3 class="catalog__title">
+      <a href="#">
+        {{ product.title }}
+      </a>
+    </h3>
+
+    <span class="catalog__price">
+      {{ product.price }} ₽
+    </span>
+
+    <ul class="colors colors--black">
+      <li class="colors__item" v-for="elColor in product.colors" :key="color">
+        <label class="colors__label">
+          <input class="colors__radio sr-only" type="radio" :value="elColor">
+          <span class="colors__value" v-bind:style="{'background-color': elColor}">
+          </span>
+        </label>
+      </li>
+     </ul>
+  </li>
+</template>
+<!-- eslint-disable linebreak-style -->
+<script>
+import colors from '../data/colors';
+
+export default {
+  data() {
+    return {
+      color: this.product.colors[0],
+    };
+  },
+  props: ['product'],
+  computed: {
+    colors() {
+      return { colors };
+    },
+  },
+};
+</script>
---
+++ src/data/colors.js
@@ -0,0 +1,31 @@
+/* eslint-disable linebreak-style */
+export default [
+  {
+    id: 1,
+    color: '#73B6EA', // Голубой
+  },
+  {
+    id: 2,
+    color: '#FFBE15', // Желтый
+  },
+  {
+    id: 3,
+    color: '#939393', // Серый
+  },
+  {
+    id: 4,
+    color: '#8BE000', // Зеленый
+  },
+  {
+    id: 5,
+    color: '#FF6B00', // Оранжевый
+  },
+  {
+    id: 6,
+    color: '#FFF', // Белый
+  },
+  {
+    id: 7,
+    color: '#000', // Черный
+  },
+];
